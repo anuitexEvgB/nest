26784ba4bd894734882c0b5890c0887fe32d0655
anuitexEvgB <yevhenii.biobrov@nure.ua>
2019-11-21 14:34:30 +0200

    aA

--- server/src/DTO/note.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ObjectID } from 'typeorm';
-
-export class NoteDto {
-    readonly id: ObjectID;
-    readonly title: string;
-    readonly text: string;
-    readonly completed: boolean;
-}
